59a098d44d3fcc95083499b763f61d7e108ae631
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-22 16:10:17 +0100

    Use cases of interfaces

--- src/oop/interfaces/Main.java
+++ src/oop/interfaces/Main.java
@@ -2,6 +2,43 @@ package oop.interfaces;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(interfaceName.PI);
+
+
+        interfaceName myInterface = new Employee();
+
+        ((Employee)myInterface).getSalary();
+
         
+
     }
+}
+
+interface interfaceName {
+    double PI = 3.14; // public static final
+
+    void something();
+
+}
+
+interface secondInterface {
+
+}
+
+// you can implement multiple interfaces!!!!
+class Employee implements interfaceName, secondInterface {
+    
+    @Override
+    public void something() {
+
+    }
+
+    private double salary;
+    public double getSalary() {
+        return this.salary;
+    }
+}
+
+class Programmer extends Employee {
+    // only one class to extends!!!!
 }
