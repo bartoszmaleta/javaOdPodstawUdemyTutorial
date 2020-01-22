29b7d9f21370251029e1361cbd1a941ca3a8d4de
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-22 13:42:36 +0100

    Add override Object.equals()

--- src/oop/classObject/Main.java
+++ src/oop/classObject/Main.java
@@ -2,9 +2,29 @@ package oop.classObject;
 
 public class Main {
     public static void main(String[] args) {
+        Object object = new Point(7, 23);
+        
         Point point = new Point(4, 10);
+        Point point2 = new Point(2, 54);
+        Point point3 = new Point(4, 10);
 
         System.out.println(point.getClass());
+
+        if (point == point2) {
+            System.out.println("Are equal???");
+        }
+        // nothing will appear, becase these are references
+
+        if (point.equals(point2)) {
+            System.out.println("point Equals point2");
+        }
+
+        if (point.equals(point3)) {
+            System.out.println("point Equals point3");
+        }
+
+
+        
     }
 }
 
@@ -28,5 +48,22 @@ class Point {
     int getY() {
         return y;
     }
+
+    // override method of Super class Object
+    @Override
+    public boolean equals(Object o) {
+        Point objectReceived = (Point) o;
+
+        // if (this.x == objectReceived.x && this.y == objectReceived.y) {
+            // return true;
+        // }
+        // 
+        // return false;
+
+        // or
+
+        return (this.x == objectReceived.x && this.y == objectReceived.y);
+    }
+    
 }
 
