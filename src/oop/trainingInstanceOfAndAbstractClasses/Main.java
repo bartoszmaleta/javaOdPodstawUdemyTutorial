fb7cde11e3ba579afa97151c4f9e46f8fef4dd2c
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-22 12:42:50 +0100

    Add abstract class and 2 extentions

--- src/oop/trainingInstanceOfAndAbstractClasses/Main.java
+++ src/oop/trainingInstanceOfAndAbstractClasses/Main.java
@@ -2,19 +2,54 @@ package oop.trainingInstanceOfAndAbstractClasses;
 
 public class Main {
     public static void main(String[] args) {
-        Person person;
+        Person[] person = new Person[4];
+        person[0] = new Employee("Bartosz", "Kowalski", 232312);
+        person[1] = new Student("John", "Smith");
+
+        person[0].getDescription();
+
     }
 }
 
 abstract class Person {
     String firstName;
     String lastName;
+
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    abstract void getDescription(); 
 }
 
 class Employee extends Person {
     double salary;
+
+    Employee(String firstName, String lastName, double salary) {
+        super(firstName, lastName);
+        this.salary = salary;
+    }
+
+    @Override
+    void getDescription() {
+System.out.println("I am employee");
+System.out.println("firstName = " + firstName);
+System.out.println("lastName = " + lastName);
+System.out.println("Salary = " + salary);
+    }
+
 }
 
 class Student extends Person {
+   Student(String firstName, String lastName) {
+       super(firstName, lastName);
+   } 
 
+   @Override void getDescription() {
+    System.out.println("I am student");
+    System.out.println("firstName = " + firstName);
+    System.out.println("lastName = " + lastName);
+    
+   }
 }
