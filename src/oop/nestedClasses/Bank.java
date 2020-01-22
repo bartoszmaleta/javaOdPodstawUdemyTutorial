5d6e330d798a3391719c8aaa56ed460d75868354
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-22 17:02:05 +0100

    Add final examples of nestedClassescd

--- src/oop/nestedClasses/Bank.java
+++ src/oop/nestedClasses/Bank.java
@@ -2,21 +2,38 @@ package oop.nestedClasses;
 
 public class Bank {
     public static void main(String[] args) {
-        Account account = new Account();
+        Account account = new Account(1000);
         
-        
+        account.start(4);
     }
 }
 
 class Account {
     private double balance;
 
+    public Account(double balance) {
+        this.balance = balance;
+    }
+
     double getBalance() {
         return this.balance;
     }
 
+    void start(double rate) {
+        Interest interest = new Interest(rate);
+        // interest.changeBalance(rate);
+    }
+
     class Interest {
         private double interestRate;
-         
+        
+        public Interest(double interestRate) {
+            this.interestRate = interestRate;
+            this.changeBalance(interestRate);
+        }
+        void changeBalance(double rate) {
+            double interestTemp = (balance * rate) / 100;
+            balance += interestTemp;
+        }
     }
 }
