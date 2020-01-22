85898d229fd463003a3ab0312257c0875d06af58
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-22 16:50:10 +0100

    Add nestesClasses

--- src/oop/nestedClasses/Main.java
+++ src/oop/nestedClasses/Main.java
@@ -2,15 +2,48 @@ package oop.nestedClasses;
 
 public class Main {
     public static void main(String[] args) {
+        A outsideClass = new A();
+        A.B nestedClassB = outsideClass.new B();
 
+        // if static, then you can create 
+        // without creating outsideClass
+        A.C nestedClassC = new A.C();
+        
     }
 }
 
 
 class A {
+    A() {
+        System.out.println("Outside class constructor");
+    }
+    
     class B {
+
+        B() {
+            System.out.println("B = Nested class constructor");
+        }
         void something() {
             test = 5;
+        }
+
+        private int temp;
+    }
+
+    void something2() {
+        // wont work, cant reach "temp" from here!!!
+        // temp = 2;
+
+        // solution:
+        B obj = new B();
+        obj.temp = 3;
+        
+        
+    }
+
+    static class C {
+        C() {
+            System.out.println("C = Nested class constructor");
         }
     }
 
